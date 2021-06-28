bc31f5e6a09883f5c5d633a83c7200c77989bac5
CristianScarella <cristianscarella09@gmail.com>
2021-06-28 18:27:22 -0300

    fixes varios y vistas de login/logout

--- src/main/java/com/projecthairdresser/app/springboothd/helpers/ViewRouteHelper.java
+++ src/main/java/com/projecthairdresser/app/springboothd/helpers/ViewRouteHelper.java
@@ -11,6 +11,9 @@ public class ViewRouteHelper {
 	
 	public static final String LOCAL_ACTUALIZAR = "local/actualizar";
 	
+	public static final String USER_LOGIN = "user/login";
+	public static final String USER_LOGOUT = "user/logout";
+	
 	//redirects
 	public final static String LOCAL_ROOT = "/local";
 	public final static String LOCAL_REDIRECT= "redirect:/local";
