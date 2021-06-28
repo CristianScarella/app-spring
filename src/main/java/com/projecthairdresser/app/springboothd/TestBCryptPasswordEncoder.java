458daad17ae799d91f5b23e6567baa5c2daf9af7
CristianScarella <cristianscarella09@gmail.com>
2021-06-28 18:25:14 -0300

    Encriptador de contraseña

---
+++ src/main/java/com/projecthairdresser/app/springboothd/TestBCryptPasswordEncoder.java
@@ -0,0 +1,12 @@
+package com.projecthairdresser.app.springboothd;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class TestBCryptPasswordEncoder {
+
+	public static void main(String[] args) {
+		BCryptPasswordEncoder pe = new BCryptPasswordEncoder();
+		System.out.println(pe.encode("empleado"));
+
+	}
+}
