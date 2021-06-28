24a42ee8d2367f5faf30284346ed408b5d664e37
CristianScarella <cristianscarella09@gmail.com>
2021-06-28 18:24:40 -0300

    permiso de eliminar local solo para admin

--- src/main/java/com/projecthairdresser/app/springboothd/controllers/LocalController.java
+++ src/main/java/com/projecthairdresser/app/springboothd/controllers/LocalController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -100,7 +101,7 @@ public class LocalController {
 		//model.addAttribute("local", local);
 		return mV;
 	}
-	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@GetMapping("/eliminar/{id}")
 	public RedirectView eliminar(@PathVariable int id, Model model) {
 		
